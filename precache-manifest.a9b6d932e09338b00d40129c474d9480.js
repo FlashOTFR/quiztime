e7da790071f2d4a1c127fbde995f3f4578cb5ad3
Randall Burgess <randallallenburgess@gmail.com>
2020-05-14 16:55:40 -0700

    Updates

---
+++ precache-manifest.a9b6d932e09338b00d40129c474d9480.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "855e26b9c20877623e0d77c49f8dce84",
+    "url": "/quiztime/index.html"
+  },
+  {
+    "revision": "7ff46a49fd0da048288c",
+    "url": "/quiztime/static/css/main.9eed83ff.chunk.css"
+  },
+  {
+    "revision": "23b2c1fde14bf78402d1",
+    "url": "/quiztime/static/js/2.45cf4c71.chunk.js"
+  },
+  {
+    "revision": "66308c03f6db8f09824bd5eec18579c5",
+    "url": "/quiztime/static/js/2.45cf4c71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ff46a49fd0da048288c",
+    "url": "/quiztime/static/js/main.5c944e37.chunk.js"
+  },
+  {
+    "revision": "925a4f47d7b7d19d305f",
+    "url": "/quiztime/static/js/runtime-main.049569b3.js"
+  },
+  {
+    "revision": "f7792cc82629e53ac22f20b5e6e5dd85",
+    "url": "/quiztime/static/media/background.f7792cc8.jpg"
+  }
+]);
